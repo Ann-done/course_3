1aed17d5d879c2104378618c79fd908a40d73691
Ann <anuta.kalganova@gmail.com>
2021-05-04 19:58:18 +0300

    login with 1 request

--- ServerET/ServerET/Controllers/ValuesController.cs
+++ ServerET/ServerET/Controllers/ValuesController.cs
@@ -23,11 +23,10 @@ namespace ServerET.Controllers
             return db.Students;
         }
 
-        // хэшировать id 
 
         [HttpPost]
         [Route("api/values/login")]
-        public Dictionary<string, dynamic> LogIn([FromBody]string json) // по номеру карты 
+        public Dictionary<string, string> LogIn([FromBody]string json) // по номеру карты 
         {
             var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
             var id = myJsonObj["id"];
@@ -36,28 +35,56 @@ namespace ServerET.Controllers
             Student student = db.Students.Find(id);
             if (student == null || !student.LastName.Equals(lastname))
             {
-                return new Dictionary<string, dynamic> { { "message", "no such student" } };
+                return new Dictionary<string, string> { { "message", "no such student" } };
             }
             if (student.IsLogIn == 1)
             {
-                return new Dictionary<string, dynamic> { { "message", "you are already logged in" } };
+                return new Dictionary<string, string> { { "message", "you are already logged in" } };
             }
             student.IsLogIn = 1;
             db.SaveChanges();
 
-            var groupId = student.GroupId;
-            Subject subject =  this.GetSubject(groupId);
-          
-            var result = new Dictionary<string, dynamic> {
+            var result = new Dictionary<string, string> {
                 {"id", student.Id.ToString() },
                 {"LastName", student.LastName },
                 {"FirstName", student.FirstName },
                 {"GroupId", student.GroupId.ToString() }
             };
 
+            Subject subject = this.FindSubject(student.GroupId);
             if (subject == null)
             {
                 result["SubjectId"] = null;
+                return result;
+            }
+
+            result["SubjectId"] = subject.Id.ToString();
+            result["SubjectShortName"] = subject.ShortName;
+            result["SubjectFullName"] = subject.FullName;
+            var i = 1;
+            foreach (var item in this.GetTopics(subject.Id))
+            {
+                result[$"Topic{i}"] = item.TopicName;
+                i++;
+            }
+
+            return result;
+        }
+
+
+        [HttpPost]
+        [Route("api/values/getsubject")]
+        public Dictionary<string,string> GetSubject([FromBody] string json) //через тело 
+        {
+            var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+            var idGroup = myJsonObj["id"];
+
+            Subject subject = this.FindSubject(idGroup);
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            if (subject == null)
+            {
+                result = null;
                 return result;
             }
             else
@@ -67,21 +94,17 @@ namespace ServerET.Controllers
                 result["SubjectFullName"] = subject.FullName;
 
                 var topics = this.GetTopics(subject.Id);
-                for ( int i=0; i < topics.Count(); i++)
+                foreach (var topic in topics)
                 {
-                    result[$"Topic{i + 1}"] = topics.ElementAt(i).TopicName; 
+                    result[$"{topic.Id}"] = topic.TopicName;
                 }
             }
+
             return result;
         }
-
-        // если авторизован (вынести часть кода в метод) 
-
-        [HttpPost]
-        [Route("api/values/getsubject/{idGroup}")]
-        public Subject GetSubject(int idGroup) //int idGroup
+        
+        private Subject FindSubject(int idGroup)
         {
-            
             Dictionary<string, int> day_lectNum = this.CheckTime();
             if ((day_lectNum == null) || (day_lectNum["LectureNum"] == 0))
             {
@@ -93,35 +116,11 @@ namespace ServerET.Controllers
             var table = from t in db.Timetables
                         where t.GroupId == idGroup && t.LectureNum == lectNum && t.DayOfWeek == day
                         select t.Subject;
-            Subject subj = table.FirstOrDefault();
-            return subj;
+            Subject subject = table.FirstOrDefault();
+            return subject;
         }
-        
 
-        //[HttpPost]
-        //[Route("api/values/getsubject")]
-        //public Subject GetSubject([FromBody]string json) //int idGroup
-        //{
-        //    var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
-        //    int idGroup = (int)myJsonObj["id"];
-
-        //    Dictionary<string, int> day_lectNum = this.CheckTime();
-        //    if ((day_lectNum == null) || (day_lectNum["LectureNum"] == 0))
-        //    {
-        //        return null;
-        //    }
-        //    var lectNum = day_lectNum["LectureNum"];
-        //    var day = day_lectNum["DayOfWeek"];
-
-        //    var table = from t in db.Timetables
-        //                where t.GroupId == idGroup && t.LectureNum == lectNum && t.DayOfWeek == day
-        //                select t.Subject;
-        //    Subject subj = table.FirstOrDefault();
-        //    return subj;
-        //}
-
-        [Route("api/values/getTopics/{idSubj}")]
-        public IEnumerable<Topic> GetTopics(int idSubj)
+        private IEnumerable<Topic> GetTopics(int idSubj)
         {
             var table = from t in db.Topics
                         where t.SubjectId == idSubj
@@ -129,18 +128,25 @@ namespace ServerET.Controllers
             return table;
         }
 
-        [Route("api/values/getQus/{idTopic}")]
-        public IEnumerable<Question> GetQus(int idTopic)
+        [HttpPost]
+        [Route("api/values/getQus")]
+        public IEnumerable<Question> GetQus([FromBody] string json)
         {
+            var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+            int idTopic = myJsonObj["id"];
             var table = from t in db.Questions
                         where t.TopicId == idTopic
                         select t;
             return table;
         }
 
-        [Route("api/values/getAnswers/{idQu}")]
-        public IEnumerable<Answer> GetAnswers(int idQu)
+        [HttpPost]
+        [Route("api/values/getAnswers")]
+        public IEnumerable<Answer> GetAnswers([FromBody]string json)
         {
+            var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+            int idQu = myJsonObj["id"];
+            
             var table = from t in db.Answers
                         where t.QuestionId == idQu
                         select t;
@@ -154,9 +160,14 @@ namespace ServerET.Controllers
             db.SaveChanges();
         }
 
-        [Route("api/values/getResults/{idSt}/{idRes}")]
-        public IEnumerable<Result> GetResults(string idSt, int idRes)
+        [HttpPost]
+        [Route("api/values/getResults")]
+        public IEnumerable<Result> GetResults([FromBody]string json)
         {
+            var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+            string idSt = myJsonObj["idSt"];
+            int idRes = myJsonObj["idRes"];  // проверять какие данные надо передавать 
+
             var table = from t in db.Results
                         where t.StudentId == idSt && t.Id > idRes
                         select t;
@@ -164,10 +175,12 @@ namespace ServerET.Controllers
             return table;
         }
 
-        [HttpGet]
-        [Route("api/values/logout/{id}")]
-        public Dictionary<string, string> LogOut(string id)
+        [HttpPost]
+        [Route("api/values/logout")]
+        public Dictionary<string, string> LogOut([FromBody] string json)
         {
+            var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+            string id = myJsonObj["id"];
             Student student = db.Students.Find(id);
             if (student == null)
             {
