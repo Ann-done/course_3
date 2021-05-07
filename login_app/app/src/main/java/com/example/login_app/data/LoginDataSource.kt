4e0ee6f9f587f9b3cbda131911a3025856544f33
Ann <anuta.kalganova@gmail.com>
2021-05-07 22:16:44 +0300

    maked hash for StCard

--- login_app/app/src/main/java/com/example/login_app/data/LoginDataSource.kt
+++ login_app/app/src/main/java/com/example/login_app/data/LoginDataSource.kt
@@ -1,17 +1,30 @@
 package com.example.login_app.data
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.login_app.data.model.LoggedInUser
 import java.io.IOException
+import java.security.spec.KeySpec
+import java.util.*
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 class LoginDataSource {
-
+    @RequiresApi(Build.VERSION_CODES.O)
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
-            // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+
+            val lastname =  username.substringBefore(" ");
+            val name = username.substringAfter(" ");
+            val cardHash = makeHash(password);
+
+            // формируем запрос
+            // TODO: здесь хэшируем пароль  и делаем запрос
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "$lastname $name")
             return Result.Success(fakeUser)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
@@ -21,4 +34,16 @@ class LoginDataSource {
     fun logout() {
         // TODO: revoke authentication
     }
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun makeHash(password: String) : String{
+
+    val salt: ByteArray = "CRYPTO".toByteArray();
+    val spec: KeySpec = PBEKeySpec(password.toCharArray(), salt, 65536, 128)
+    val f: SecretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+    val hash: ByteArray = f.generateSecret(spec).getEncoded()
+    val enc: Base64.Encoder = Base64.getEncoder()
+    return enc.encodeToString(hash);
 }
