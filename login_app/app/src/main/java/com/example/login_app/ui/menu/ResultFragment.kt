9e95af131a581224c20b09c339edfc23fe292b23
MrNeither <mr.neitherman@gmail.com>
2021-05-15 17:56:30 +0300

    add view list ub Result Fragment

--- login_app/app/src/main/java/com/example/login_app/ui/menu/ResultFragment.kt
+++ login_app/app/src/main/java/com/example/login_app/ui/menu/ResultFragment.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.login_app.R
 
 class ResultFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.activity_result, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.activity_result, container, false)
+
+        val lv_list = root.findViewById<ListView>(R.id.LV_results)
+
+        val activity: MenuActivity? = activity as MenuActivity?
+        val listResults = activity?.getResults()
+
+        if (listResults != null) {
+            val adapter = ViewAdapter(root.context, listResults)
+            lv_list.adapter = adapter
+        }
+        return root
     }
-}// Required empty public constructor
+}
